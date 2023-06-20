a28f57b9e66ce2767801ec8af0e670b330608424
CamGold <camgoldbiz@gmail.com>
2023-06-20 09:46:36 +0200

    Final FizzBuzz With Tests
    Finished Tests and FizzBuzz Logic

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.FizzBuzz(15);
+            
+        }
+
+        public void FizzBuzz(int range)
+        {
+            var numbers = new List<string>();
+            for (int i = 1; i < range + 1; i++) 
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    numbers.Add("FizzBuzz");
+                }
+                else if(i %3 == 0)
+                {
+                    numbers.Add("Fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    numbers.Add("Buzz");
+                }
+                else
+                {
+                    numbers.Add(i.ToString());
+                }
+            }
+            numbers.ForEach(i => Console.Write(i + " "));
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTests/ProgramTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System.IO;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void rangeOf3Test()
+        {
+            StringWriter stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            Program program = new Program();
+            program.FizzBuzz(3);
+
+            string output = stringWriter.ToString();
+
+            Assert.AreEqual("1 2 Fizz ", output);
+        }
+
+        [TestMethod()]
+        public void rangeOf15Test()
+        {
+            StringWriter stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            Program program = new Program();
+            program.FizzBuzz(15);
+
+            string output = stringWriter.ToString();
+
+            Assert.AreEqual("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz ", output);
+        }
+    }
+}
